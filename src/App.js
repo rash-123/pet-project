dcb812c9748f364d9e485b55792506c1cb57f541
Rashmi Verma <vermarashmi559@gmail.com>
2021-08-08 19:27:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import SearchParams from './SearchParams';
+import Details from './Details';
+import ThemeContext from './ThemeContext';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const theme = useState("darkblue");
+    return (
+        <ThemeContext.Provider value={theme}>
+            <div>
+                <Router>
+                    <header>
+                        <Link to="/">
+                            <h1>Adopt Me!</h1>
+                        </Link>
+                    </header>
+                    <Switch>
+                        <Route path="/details/:id">
+                            <Details />
+                        </Route>
+                        <Route path="/">
+                            <SearchParams />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        </ThemeContext.Provider>
+    );
+};
 
 export default App;
